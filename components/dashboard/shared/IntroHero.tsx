db410025de272aa1f928b2627798e0097e09e961
José Brito <josealbritose@gmail.com>
2024-12-09 02:48:34 -0300

    Intro

---
+++ components/dashboard/shared/IntroHero.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function IntroHero() {
+  return (
+    <section className="max-w-xl mx-auto space-y-8">
+      <h1 className="text-3xl md:text-4xl font-extrabold text-center">
+        🛠️ Implement your solution here! 🚀
+      </h1>
+      <p className="text-lg text-gray-600 text-center">
+        This is where you can implement the search bar and pagination for the
+        technical test. Make it efficient, shareable, and user-friendly!
+      </p>
+      <p className="text-center text-sm text-gray-500 italic">
+        Remember, we’re looking for clean and optimized code. 💡
+      </p>
+    </section>
+  );
+}
